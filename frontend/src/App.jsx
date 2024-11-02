e603e80eb650b0b4736af02aecf01065d35743d7
Cammille Greene <96968550+Cammille-Mitchell@users.noreply.github.com>
2024-11-02 16:57:17 -0400

    navbar, registration and login forms upated.  page linking from navbar not implmemented yet.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import Navbar from "./Components/Navbar";
+import Login from "./Components/Login";
+import Registration from "./Components/Registration";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [count, setCount] = useState(0);
   return (
-    <>
-      <div>
-        <h1>Travel App</h1>
+    <div className="App">
+      <Navbar />
+      <div className="content">
+        <Login />
+        <Registration />
       </div>
-    </>
-  )
+    </div>
+  );
 }
-
-export default App
+export default App;
