a90dbce247b47c4b9145ec877877054f67f7311c
cfeliciano <cfeliciano7792@gmail.com>
2024-11-05 18:59:52 -0500

    .

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,8 +1,3 @@
-// ORIGINAL FUNCTIONING VERSION
-import React from "react";
-import { Link } from "react-router-dom";
-
-
 import React from "react";
 import { Link } from "react-router-dom";
 const Navbar = () => {
