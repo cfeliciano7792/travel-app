f211a0478d3646ce0dcd0e0b8d7373148cffd8ef
cfeliciano <cfeliciano7792@gmail.com>
2024-11-16 22:03:46 -0500

    Connect registration form to backend. Added navigation to home page af registration was successful.

--- frontend/src/Components/Registration.jsx
+++ frontend/src/Components/Registration.jsx
@@ -1,31 +1,50 @@
 // code reference: https://www.youtube.com/watch?v=8hU0I8rY4u4
-import { useEffect, useState } from "react";
+import { useEffect, useState} from "react";
+import { useNavigate } from "react-router-dom";
 
 const Registration = () => {
   const [values, setValues] = useState({
-    firstName: "",
-    lastName: "",
+    username: "",
     email: "",
+    password: "",
   });
   // ensures that the successful registration message pops up after form is submitted
   const [submitted, setSubmitted] = useState(false);
 
-  const handleFirstNameInputChange = (event) => {
-    setValues({ ...values, firstName: event.target.value });
+  const handleUserNameInputChange = (event) => {
+    setValues({ ...values, username: event.target.value });
   };
 
-  const handleLastNameInputChange = (event) => {
-    setValues({ ...values, lastName: event.target.value });
+  const handlePasswordInputChange = (event) => {
+    setValues({ ...values, password: event.target.value });
   };
 
   const handleEmailInputChange = (event) => {
     setValues({ ...values, email: event.target.value });
   };
 
+  let navigate = useNavigate()
+
+  const userData = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(values)
+  }
+
   // ensures that when the page refreshes, the data isn't lost
   const handleSubmit = (event) => {
     event.preventDefault();
-    setSubmitted(true);
+    console.log(userData)
+    fetch('http://localhost:5000/api/signup', userData)
+    .then(response => response.json())
+    .then(data => {
+      console.log("Signup successful:", data);
+      navigate(`/home`); 
+  })
+    .catch(error => console.error(error));
+    
   };
 
   return (
@@ -36,26 +55,27 @@ const Registration = () => {
           <div className="successful-reg">Registration Completed!</div>
         ) : null}
         <input
-          onChange={handleFirstNameInputChange}
+          onChange={handleUserNameInputChange}
           value={values.firstName}
           className="form-field"
-          placeholder="First Name"
+          placeholder="Username"
           name="firstName"
           required
         />
         <input
-          onChange={handleLastNameInputChange}
+          onChange={handlePasswordInputChange}
           value={values.lastName}
           className="form-field"
-          placeholder="Last Name"
-          name="lastName"
+          placeholder="Password"
+          name="password"
           required
         />
         <input
           onChange={handleEmailInputChange}
+          type='email'
           value={values.email}
           className="form-field"
-          placeholder="Email"
+          placeholder="traveler@example.com"
           name="Email"
           required
         />
