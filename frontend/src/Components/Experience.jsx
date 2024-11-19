eff691478a1fda12dedb081b70139f68aa2ea21f
cfeliciano <cfeliciano7792@gmail.com>
2024-11-19 18:01:37 -0500

    added styling

--- frontend/src/Components/Experience.jsx
+++ frontend/src/Components/Experience.jsx
@@ -4,12 +4,14 @@ const Experience = ({experience}) => {
 
     
     return (
-        <div onClick={() => addExperienceToTrip()} className="flex flex-col border-2 border-black mt-4 p-4">
+        <div onClick={() => addExperienceToTrip()} className="flex flex-col border-2 border-black mt-4 p-4 max-w-3xl">
             {experience.title}
             <div>
             {experience.rating}/5
             </div>
+            <div className="content-start mx-auto text-left">
             {experience.description}
+            </div>
         </div>
     )
 };
