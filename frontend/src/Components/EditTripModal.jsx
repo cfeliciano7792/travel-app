be68d213ca14f1720bbaf52929f9bd33391b743a
Cammille Greene <96968550+Cammille-Mitchell@users.noreply.github.com>
2024-12-01 21:14:40 -0500

    updated mytrips page to display the description/date for a given trip. resolved null value in trip date parameter. changed button colors.

--- frontend/src/Components/EditTripModal.jsx
+++ frontend/src/Components/EditTripModal.jsx
@@ -10,7 +10,7 @@ function EditTripModal({onClose, trip}) {
         trip_id: trip.trip_id,
         title: "",
         description: "",
-        dates: "",
+        trip_date: "",
         
     });
     
@@ -24,7 +24,7 @@ function EditTripModal({onClose, trip}) {
     };
 
     const handleDatesInputChange = (event) => {
-        setValues({ ...values, dates: event.target.value });
+        setValues({ ...values, trip_date: event.target.value });
         };
 
 
@@ -65,8 +65,8 @@ function EditTripModal({onClose, trip}) {
                                 <input value={values.description} type="text" id="description" className="bg-gray-50 border-emerald-600 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" onChange={handleDescriptionInputChange} />
                             </div>
                             <div>
-                                <label htmlFor="dates" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Travel Dates:</label>
-                                <input value={values.dates} type="date" id="dates" className="bg-gray-50 border-emerald-600text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" onChange={handleDatesInputChange} />
+                                <label htmlFor="trip_date" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Travel Dates:</label>
+                                <input value={values.trip_date} type="date" id="dates" className="bg-gray-50 border-emerald-600text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" onChange={handleDatesInputChange} />
                             </div>
                         </div>
                         <button type="submit" className="text-white bg-emerald-500 hover:bg-emerald-700focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
--- frontend/src/Components/Trip.jsx
+++ frontend/src/Components/Trip.jsx
@@ -35,18 +35,25 @@ const Trip = ({trip}) => {
         <div className="flex flex-col text-xl bg-white border-2 border-black mt-4 p-4 max-w-3xl hover:border-4 hover:shadow-2xl hover:border-blue-700">
             {/* Restrict the Link to only wrap the title */}
             <Link to={`/mytrips/${trip.trip_id}`} state={trip}>
-                <div className="text-blue-600 hover:underline">{trip.title}</div>
+                <div className="text-blue-600 hover:underline">{trip.title}
+                </div>
             </Link>
+            <div>
+                {trip.description}
+            </div>
+            <div> 
+                {trip.trip_date}
+            </div>
             {/* Buttons */}
             <div>
                 <button
-                    className="mt-2 mx-2 text-white bg-emerald-500 hover:bg-emerald-700 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+                    className="mt-2 mx-2 text-white bg-rose-500 hover:bg-rose-700 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
                     onClick={handleDeleteClick}
                 >
                     DELETE
                 </button>
                 <button
-                    className="mt-2 text-white bg-emerald-500 hover:bg-emerald-700 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+                    className="mt-2 text-white bg-blue-300 hover:bg-blue-500 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
                     onClick={handleEditClick}
                 >
                     EDIT
